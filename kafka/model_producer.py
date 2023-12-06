8816f42c394074ecd078bedcbf5aee9a5947d7ce
Bartosz Rzycki <125658269+barto-official@users.noreply.github.com>
2023-12-06 19:42:08 +0100

    delete kafka old code, add folder for asynchronous app

--- kafka/model_producer.py
+++
@@ -1,7 +0,0 @@
-from confluent_kafka import Producer
-
-def produce_results(prediction, conf, topic):
-    producer=Producer(conf)
-    producer.produce(topic, key=str('prediction'), value=str(prediction))
-    producer.flush()
-    print(prediction, "sent to kafka (!)")
